3819ee0a04eeb5bab7a3f046626c60ae36571be9
Mauro Palsgraaf <mauro@openvalue.nl>
2018-06-26 14:35:03 +0200

    initial commit

---
+++ src/main/java/com/ing/pacman/PacmanClient/PacmanClientApplication.java
@@ -0,0 +1,12 @@
+package com.ing.pacman.PacmanClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacmanClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PacmanClientApplication.class, args);
+	}
+}
